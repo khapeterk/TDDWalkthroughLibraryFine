731420e998f056a559751264b02112af7a8e7410
khapeterk <khapeterk@gmail.com>
2018-01-09 09:42:47 -0500

    wrote first test expectation

--- spec/library-fine.spec.js
+++ spec/library-fine.spec.js
@@ -1,5 +1,5 @@
 describe("a function for Library Fine", () => {
   it("should return 0 for book returned before due date", () => {
-    
+    expect(libraryFine("1 8 2018\n1 9 2018")).toBe(0)
   })
 })
