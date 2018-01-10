889d94fdb9514f2f5742f03f7420f34a2afadeba
khapeterk <khapeterk@gmail.com>
2018-01-10 16:18:03 -0500

    added second test

--- spec/library-fine.spec.js
+++ spec/library-fine.spec.js
@@ -2,4 +2,7 @@ describe("a function for Library Fine", () => {
   it("should return 0 for book returned before due date", () => {
     expect(libraryFine("1 8 2018\n1 9 2018")).toBe(0)
   })
+  it("should return 0 for book returned on due date", () => {
+    expect(libraryFine("1 8 2018\n1 8 2018")).toBe(0)
+  })
 })
