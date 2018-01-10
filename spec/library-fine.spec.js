133b74f7cac06445b162041165f73d6c38529d84
khapeterk <khapeterk@gmail.com>
2018-01-10 18:02:00 -0500

    added new case for second test

--- spec/library-fine.spec.js
+++ spec/library-fine.spec.js
@@ -6,4 +6,7 @@ describe("a function for Library Fine", () => {
   it("should return 15 for book returned one day late", () => {
     expect(libraryFine("1 8 2018\n1 7 2018")).toBe(15)
   })
+  it("should return 30 for book returned two days late", () => {
+    expect(libraryFine("1 8 2018\n1 6 2018")).toBe(30)
+  })
 })
