65fbb895957f452310d189f4827aebf779917b8a
khapeterk <khapeterk@gmail.com>
2018-01-11 18:05:56 -0500

    generalized if conditional

--- src/library-fine.js
+++ src/library-fine.js
@@ -4,8 +4,6 @@ function libraryFine(dates) {
   dueDate = datesArray[1]
   returnDay = parseInt(returnDate[2])
   dueDay = parseInt(dueDate[2])
-  if (dates === "1 8 2018\n1 6 2018" || dates === "1 8 2018\n1 7 2018") {
-    return (returnDay - dueDay) * 15
-  }
+  if (returnDay > dueDay) return (returnDay - dueDay) * 15
   return 0
 }
