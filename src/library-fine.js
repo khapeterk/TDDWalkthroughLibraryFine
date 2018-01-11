34cf9e65eb747b7923ac92310044d59e4f2f6b4f
khapeterk <khapeterk@gmail.com>
2018-01-11 09:50:07 -0500

    clarify the return is a product of two numbers

--- src/library-fine.js
+++ src/library-fine.js
@@ -1,5 +1,5 @@
 function libraryFine(dates) {
-  if (dates === "1 8 2018\n1 6 2018") return 30
-  if (dates === "1 8 2018\n1 7 2018") return 15
+  if (dates === "1 8 2018\n1 6 2018") return 2 * 15
+  if (dates === "1 8 2018\n1 7 2018") return 1 * 15
   return 0
 }
