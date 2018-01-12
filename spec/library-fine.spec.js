b43fed72a56d8fa0a323832fdfd8c5082d16e4ce
khapeterk <khapeterk@gmail.com>
2018-01-12 01:23:52 -0500

    added fourth test's case

--- spec/library-fine.spec.js
+++ spec/library-fine.spec.js
@@ -10,4 +10,7 @@ describe("a function for Library Fine", () => {
   it("should return fee for overdue book in same year", () => {
     expect(libraryFine("2 8 2018\n1 8 2018")).toBe(500)
   })
+  it("should return flat fee for overdue book in different year", () => {
+    expect(libraryFine("1 1 2019\n1 8 2018")).toBe(10000)
+  })
 })
