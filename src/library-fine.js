2965ca0e52ee08b67012ea87ed92207ba2d3400c
khapeterk <khapeterk@gmail.com>
2018-01-12 01:33:40 -0500

    implemented fourth rule

--- src/library-fine.js
+++ src/library-fine.js
@@ -2,10 +2,13 @@ function libraryFine(dates) {
   datesArray = dates.split("\n")
   returnDate = datesArray[0]
   dueDate = datesArray[1]
+  returnYear = parseInt(returnDate.substr(4, 4))
+  dueYear = parseInt(dueDate.substr(4, 4))
   returnMonth = parseInt(returnDate[0])
   dueMonth = parseInt(dueDate[0])
   returnDay = parseInt(returnDate[2])
   dueDay = parseInt(dueDate[2])
+  if(returnYear > dueYear) return 10000
   if (returnMonth > dueMonth) return (returnMonth - dueMonth) * 500
   if (returnDay > dueDay) return (returnDay - dueDay) * 15
   return 0
