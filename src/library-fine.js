0d5e96d9eaa3ffd0542e30cd52af0b77b5ec55a5
khapeterk <khapeterk@gmail.com>
2018-01-12 01:12:14 -0500

    obvious implementation of third test

--- src/library-fine.js
+++ src/library-fine.js
@@ -2,8 +2,11 @@ function libraryFine(dates) {
   datesArray = dates.split("\n")
   returnDate = datesArray[0]
   dueDate = datesArray[1]
+  returnMonth = parseInt(returnDate[0])
+  dueMonth = parseInt(dueDate[0])
   returnDay = parseInt(returnDate[2])
   dueDay = parseInt(dueDate[2])
+  if (returnMonth > dueMonth) return (returnMonth - dueMonth) * 500
   if (returnDay > dueDay) return (returnDay - dueDay) * 15
   return 0
 }
